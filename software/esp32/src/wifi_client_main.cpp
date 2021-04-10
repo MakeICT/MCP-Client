0b25bb90755a428e13aad6d22506399d77071d81
Christian Kindel <iceman81292@gmail.com>
2021-04-10 18:15:15 -0500

    remove commented SSL cert code which was transferred to mcp_link.h

--- software/esp32/src/wifi_client_main.cpp
+++ software/esp32/src/wifi_client_main.cpp
@@ -109,24 +109,6 @@ Network network = Network();
 MCPLink server = MCPLink();
 LEDs lights = LEDs();
 
-/* Letsencrypt root cert, taken from server_root_cert.pem
-
-   The PEM file was extracted from the output of this command:
-   openssl s_client -showcerts -connect www.howsmyssl.com:443 </dev/null
-
-   The CA root cert is the last cert given in the chain of certs.
-
-   To embed it in the app binary, the PEM file is named
-   in the component.mk COMPONENT_EMBED_TXTFILES variable.
-*/
-// extern const uint8_t server_root_cert_pem_start[] asm("_binary_server_root_cert_pem_start");
-// extern const uint8_t server_root_cert_pem_end[]   asm("_binary_server_root_cert_pem_end");
-
-// static const char *REQUEST = "POST " AUTH_ENDPOINT "?email=" CONFIG_USERNAME    "&password=" CONFIG_PASSWORD "\r\n"
-//     "Host: " WEB_SERVER "\r\n"
-//     "Content-Type: application/x-www-form-urlencoded\r\n"
-//     "\r\n";
-
 lightPattern *system_start_pattern = new lightPattern();
 lightPattern *no_network_pattern = new lightPattern();
 lightPattern *alarm_armed_pattern = new lightPattern();
